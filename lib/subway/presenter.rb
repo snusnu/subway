35d69c5da6eef7f9025e727349f8186cf385905d
snusnu <gamsnjaga@gmail.com>
2013-10-16 01:27:48 +0200

    Add Subway::Presenter extracted from our app

---
+++ lib/subway/presenter.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+
+module Subway
+
+  class Presenter
+    include AbstractType
+    include Concord.new(:data)
+    include Adamantium::Flat
+
+    def method_missing(method, *args, &block)
+      @data.send(method, *args, &block)
+    end
+
+    def respond_to?(method, include_private = false)
+      super || @data.respond_to?(method, include_private)
+    end
+
+    class Collection < self
+      include Enumerable
+
+      def self.member(presenter = Undefined)
+        return @member if presenter.equal?(Undefined)
+        @member = presenter
+      end
+
+      alias_method :entries, :data
+
+      protected :entries
+
+      def each
+        return to_enum unless block_given?
+        entries.each { |entry| yield self.class.member.new(entry) }
+        self
+      end
+    end
+  end
+end
