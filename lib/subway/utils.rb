d079d8d78fdc46773f577ab079a0db58bd27d5f3
snusnu <gamsnjaga@gmail.com>
2013-11-14 16:17:21 +0100

    Nuke Anima#to_h (now available in anima proper)

--- lib/subway/utils.rb
+++ lib/subway/utils.rb
@@ -5,11 +5,5 @@ module Subway
       Dir[root.join(pattern)].each { |file| require file }
     end
 
-    module Anima
-      def to_h
-        self.class.attributes_hash(self)
-      end
-    end # module Anima
-
   end # module Utils
 end # module Subway
