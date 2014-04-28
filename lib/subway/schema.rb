55c893feffb901453c615cef2abf4c853f134fa0
snusnu <gamsnjaga@gmail.com>
2014-04-28 13:45:04 +0200

    Add experimental relation schema
    This is mostly a safepoint for the stuff i currently have. It’s very
    likely that this will be rewritten and enhanced to use alf-core under
    the hood.

---
+++ lib/subway/schema.rb
@@ -0,0 +1,230 @@
+# encoding: utf-8
+
+require 'dm-core'
+require 'axiom'
+require 'axiom-types'
+require 'axiom-optimizer'
+require 'axiom-do-adapter'
+
+module Subway
+  class Relation
+
+    class Builder
+
+      include AbstractType
+      include Procto.call
+
+      abstract_method :name
+      abstract_method :header
+
+      def call
+        Axiom::Relation::Base.new(name, header)
+      end
+
+      class DM < self
+
+        include Concord.new(:model)
+
+        def name
+          model.storage_names[:default].to_sym
+        end
+
+        def header
+          model.properties.map { |p| [p.field, p.primitive] }
+        end
+      end # DataMapper
+    end # Builder
+
+    class Schema
+      class Builder
+
+        class DM < self
+
+          include Concord.new(:models, :relation_builder)
+          include Procto.call
+
+          def initialize(models, relation_builder = Relation::Builder::DM)
+            super
+          end
+
+          def call
+            models.each_with_object({}) { |model, hash|
+              relation = relation_builder.call(model)
+              hash[relation.name] = relation
+            }
+          end
+
+        end
+
+        class Context
+
+          include Concord::Public.new(:relations)
+
+          def self.call(relations = {}, &block)
+            new(relations, &block).relations
+          end
+
+          def initialize(relations, &block)
+            super
+            instance_eval(&block)
+          end
+
+          private
+
+          def relation(name, &block)
+            relations[name] = Schema::Context.call(relations, block)
+          end
+        end # Context
+
+        include Adamantium::Flat
+        include Concord.new(:relations, :block)
+        include Procto.call
+
+        def call
+          Schema.new(Context.call(relations, &block))
+        end
+      end # Builder
+
+      class Context < BasicObject
+
+        def self.call(relations, block)
+          new(relations, block).call
+        end
+
+        def initialize(relations, block)
+          @relations, @block = relations, block
+        end
+
+        def call
+          instance_eval(&@block).optimize
+        end
+
+        def inspect
+          @relations.inspect
+        end
+
+        private
+
+        def method_missing(name, *args, &block)
+          super unless @relations.key?(name)
+          @relations[name]
+        end
+
+        def respond_to_missing?(name, include_private = false)
+          @relations.key?(name) or super
+        end
+      end # Context
+
+      include Lupo.collection(:relations)
+
+      def self.build(relations, &block)
+        Builder.call(relations, block)
+      end
+
+      def [](name)
+        relations.fetch(name)
+      end
+    end # Schema
+
+    include Concord.new(:relation, :mapper)
+    include Enumerable
+
+    def each(&block)
+      return to_enum unless block
+      relation.each { |tuple| yield(mapper.load(tuple)) }
+      self
+    end
+
+    def sort
+      new(relation.sort)
+    end
+
+    def sort_by(*args, &block)
+      new(relation.sort_by(*args, &block))
+    end
+
+    def one(&block)
+      mapper.load(relation.one(&block))
+    end
+
+    def restrict(*args, &block)
+      new(relation.restrict(*args, &block))
+    end
+
+    def wrap(*args)
+      new(relation.wrap(*args))
+    end
+
+    def group(*args)
+      new(relation.group(*args))
+    end
+
+    private
+
+    def new(new_relation)
+      self.class.new(new_relation, mapper)
+    end
+  end # Relation
+
+  class Database
+
+    include Concord.new(:name, :adapter, :relation_schema)
+
+    def self.build(name, adapter, schema)
+      new(name, adapter, relations(adapter, schema))
+    end
+
+    def self.relations(adapter, schema)
+      schema.each_with_object({}) { |(name, relation), hash|
+        hash[name] = Axiom::Relation::Gateway.new(adapter, relation)
+      }
+    end
+
+    def [](name)
+      relation_schema.fetch(name)
+    end
+
+  end # Database
+
+  class Schema
+
+    class Definition
+
+      def self.call(relations, mappers, &block)
+        new(relations, mappers, &block).entries
+      end
+
+      attr_reader :entries
+
+      def initialize(relations, mappers, &block)
+        @relations, @mappers = relations, mappers
+        @entries = {}
+        instance_eval(&block) if block
+      end
+
+      def map(relation_name, mapper_name)
+        entries[relation_name] = relation(relation_name, mapper_name)
+      end
+
+      def relation(relation_name, mapper_name)
+        Relation.new(relations[relation_name], mappers.mapper(mapper_name))
+      end
+
+      private
+
+      attr_reader :relations
+      attr_reader :mappers
+
+    end # Definition
+
+    include Concord.new(:relations)
+
+    def self.build(relations, mappers, &block)
+      new(Definition.call(relations, mappers, &block))
+    end
+
+    def [](relation_name)
+      relations[relation_name]
+    end
+  end # Schema
+end # Subway
---
+++ spec/integration/schema_spec.rb
@@ -0,0 +1,227 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+require 'dm-core'
+require 'dm-migrations'
+
+require 'axiom'
+require 'axiom-do-adapter'
+
+require 'subway/entity'
+require 'subway/schema'
+
+describe Subway::Schema do
+
+  # (1) Setup and define tables with DataMapper
+
+  uri = 'postgres://localhost/test'.freeze
+
+  DataMapper.logger = DataMapper::Logger.new($stdout, :debug) if ::ENV['LOG']
+  DataMapper.setup(:default, uri)
+
+  class Account
+    include DataMapper::Resource
+
+    property :id,    Serial, field: 'account_id'
+    property :email, String
+  end
+
+  class Person
+    include DataMapper::Resource
+
+    property :id,   Serial, field: 'person_id'
+    property :name, String
+
+    belongs_to :account
+    has n, :tasks
+  end
+
+  class Task
+    include DataMapper::Resource
+
+    property :id,   Serial, field: 'task_id'
+    property :name, String
+
+    belongs_to :person
+  end
+
+  DataMapper.finalize.auto_migrate!
+
+  Person.create(
+    name:    'snusnu',
+    account: { email: 'test@test.com' },
+    tasks:   [{name: 'test'}]
+  )
+
+  # (2) Initialize a new Subway::Relation::Schema
+
+  models          = DataMapper::Model.descendants
+  base_relations  = Subway::Relation::Schema::Builder::DM.call(models)
+
+  relation_schema = Subway::Relation::Schema.build(base_relations) do
+
+    relation :actors do
+      people.join(accounts)
+    end
+
+    relation :person_details do
+      actors.join(tasks.rename(name: :task_name))
+    end
+
+    relation :task_details do
+      tasks.
+        join(people.rename(name: :person_name)).
+        join(accounts)
+    end
+
+  end
+
+  # (3) Connect the relation schema to a database
+
+  adapter  = Axiom::Adapter::DataObjects.new(uri)
+  database = Subway::Database.build(:test, adapter, relation_schema)
+
+  # (4) Define domain DTOs
+
+  entity_registry = Subway::Entity::Definition::Registry.build(guard: false) do
+
+    register :account do
+      map :id,    :Integer, from: :account_id
+      map :email, :String
+    end
+
+    register :person do
+      map :id,         :Integer, from: :person_id
+      map :account_id, :Integer
+      map :name,       :String
+    end
+
+    register :task do
+      map :id,   :Integer, from: :task_id
+      map :name, :String
+    end
+
+    register :detailed_person do
+      map :id,         :Integer, from: :person_id
+      map :name,       :String
+
+      wrap :account, entity: :'detailed_person.account' do
+        map :id,    :Integer, from: :account_id
+        map :email, :String
+      end
+
+      group :tasks, entity: :'detailed_person.task' do
+        map :id,   :Integer, from: :task_id
+        map :name, :String,  from: :task_name
+      end
+    end
+
+    register :detailed_task do
+      map :id,         :Integer, from: :task_id
+      map :name,       :String
+
+      wrap :person, entity: :'task.person' do
+        map :id,   :Integer, from: :person_id
+        map :name, :String,  from: :person_name
+
+        wrap :account, entity: :'task.person.account' do
+          map :id,    :Integer, from: :account_id
+          map :email, :String
+        end
+      end
+    end
+
+    register :actor do
+      map :id,         :Integer, from: :person_id
+      map :name,       :String
+
+      wrap :account, entity: :'actor.account' do
+        map :id,       :Integer, from: :account_id
+        map :email,    :String
+      end
+    end
+
+  end
+
+  models   = entity_registry.models(:anima)
+  entities = entity_registry.environment(models)
+
+  # (5) Connect schema relations with DTO mappers
+
+  schema = Subway::Schema.build(database, entities) do
+    map :accounts,       :account
+    map :people,         :person
+    map :tasks,          :task
+    map :person_details, :detailed_person
+    map :task_details,   :detailed_task
+    map :actors,         :actor
+  end
+
+  it 'provides access to axiom gateway base relations' do
+
+    account = schema[:accounts].sort.one
+    expect(account.id).to_not be(nil)
+    expect(account.email).to eq('test@test.com')
+
+    person = schema[:people].sort.one
+    expect(person.id).to_not be(nil)
+    expect(person.name).to eq('snusnu')
+    expect(person.account_id).to eql(account.id)
+
+    schema[:accounts].each do |object|
+      expect(object.id).to_not be(nil)
+      expect(object.email).to eq('test@test.com')
+    end
+
+    schema[:people].each do |object|
+      expect(object.id).to_not be(nil)
+      expect(object.name).to eq('snusnu')
+      expect(object.account_id).to eql(account.id)
+    end
+  end
+
+  it 'provides access to axiom gateway relations' do
+    account = schema[:accounts].sort.one
+    person  = schema[:people].sort.one
+    task    = schema[:tasks].sort.one
+
+    schema[:actors].wrap(account: [:account_id, :email]).each do |actor|
+      expect(actor.id).to eq(person.id)
+      expect(actor.name).to eq(person.name)
+      expect(actor.account.id).to eq(account.id)
+      expect(actor.account.email).to eq(account.email)
+    end
+
+    task_details = schema[:task_details].
+      wrap(
+        account: [:account_id, :email],
+        person:  [:person_id, :person_name, :account]
+      )
+
+    task_details.each do |detailed_task|
+      expect(detailed_task.id).to eq(task.id)
+      expect(detailed_task.name).to eq(task.name)
+      expect(detailed_task.person.id).to eq(person.id)
+      expect(detailed_task.person.name).to eq(person.name)
+      expect(detailed_task.person.account.id).to eql(account.id)
+      expect(detailed_task.person.account.email).to eql(account.email)
+    end
+
+    person_details = schema[:person_details].
+      group(tasks: [:task_id, :task_name]).
+      wrap(account: [:account_id, :email])
+
+    person_details.each do |detailed_person|
+      expect(detailed_person.id).to eq(person.id)
+      expect(detailed_person.name).to eq(person.name)
+      expect(detailed_person.account.id).to eq(account.id)
+      expect(detailed_person.account.email).to eq(account.email)
+
+      detailed_person.tasks.each do |person_task|
+        expect(person_task.id).to eq(task.id)
+        expect(person_task.name).to eq(task.name)
+      end
+    end
+  end
+end
