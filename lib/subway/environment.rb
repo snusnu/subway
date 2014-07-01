da47e3cc8954f74639e843724b2a071fb3abf9b3
snusnu <gamsnjaga@gmail.com>
2014-07-01 22:56:05 +0200

    Add extracted Environment

---
+++ lib/subway/environment.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+module Subway
+  class Environment
+
+    include Concord.new(:name, :services)
+
+    public :name
+
+    def self.new(name, services = EMPTY_HASH)
+      super(name.to_sym, services.dup)
+    end
+
+    def register(other_services)
+      self.class.new(services.merge(other_services))
+    end
+
+    private
+
+    def method_missing(name, *args, &block)
+      services.fetch(name, super)
+    end
+
+    def respond_to_missing?(name, include_private = false)
+      super || services.key?(name)
+    end
+  end # Environment
+end # Subway
